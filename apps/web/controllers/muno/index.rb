b9397dbce1537158d10a0ed2f7e5382c20bfe39d
akinomaeni <akinomaeni@gmail.com>
2016-03-30 21:49:16 +0900

    good bye hanami

--- apps/web/controllers/muno/index.rb
+++
@@ -1,8 +0,0 @@
-module Web::Controllers::Muno
-  class Index
-    include Web::Action
-
-    def call(param)
-    end
-  end
-end
--- apps/web/views/muno/index.rb
+++
@@ -1,5 +0,0 @@
-module Web::Views::Muno
-  class Index
-    include Web::View
-  end
-end
