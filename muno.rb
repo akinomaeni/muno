026a3a9a1ab4ff5b2577a211ebf5913369bdb0ea
akinomaeni <akinomaeni@gmail.com>
2016-04-04 09:29:21 +0900

    Top page

--- muno.rb
+++ muno.rb
@@ -1,5 +1,19 @@
+require 'haml'
+
 class MunoApp
   def call(env)
-    [200, {}, ["Hello World!"]]
+    request = Rack::Request.new(env)
+    if request.get? && request.path == '/'
+      Rack::Response.new(render('home/index.html.haml'))
+    else
+      Rack::Response.new('404 Not Found.', '404')
+    end
+  end
+
+  def render(template)
+    path = File.expand_path("../app/views/#{template}", __FILE__)
+    template = File.read(path)
+    haml_engine = Haml::Engine.new(template)
+    haml_engine.render
   end
 end
